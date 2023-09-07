66e2e9d0051ae08090a9eb701c64dbcfdbe8e9c7
mbaibatyr <murat_b@mail.ru>
2023-09-07 21:44:36 +0600

    -

--- MyCRUD_MVC/Models/Country.cs
+++ MyCRUD_MVC/Models/Country.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +10,12 @@ namespace MyCRUD_MVC.Models
     public class Country
     {
         public int id { get; set; }
+        [DisplayName("Страна"), Required(ErrorMessage = "Страна обязательное поле")]        
         public string name { get; set; }
+        //[DisplayName("Столица"), Required, MaxLength(100), MinLength(3), EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [DisplayName("Столица"), EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string capital { get; set; }
+        [DisplayName("Население"), Required, Range(1000, 1000000)]
         public int population { get; set; }
     }
 
