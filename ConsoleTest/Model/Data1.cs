202d1aab260f7a5fae00333b45da585664bcc809
mbaibatyr <murat_b@mail.ru>
2023-09-26 20:23:35 +0600

    --

---
+++ ConsoleTest/Model/Data1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest.Model
+{
+    internal class Data1
+    {
+    }
+
+
+    public class Rootobject
+    {
+        public string Name { get; set; }
+        public DataArray[] Data { get; set; }
+    }
+
+    public class DataArray
+    {
+        public string name { get; set; }
+        public string language { get; set; }
+        public string id { get; set; }
+        public string bio { get; set; }
+        public float version { get; set; }
+    }
+
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleTest.Model;
+using Newtonsoft.Json;
+//using System.Text.Json;
+//using System.Text.Json.Serialization;
+using System.Data;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText(@"C:\Users\байбатыровм\Desktop\json_sample_2.json");
+            //DataTable dt = JsonConvert.DeserializeObject<DataTable>(json);
+            //foreach (DataRow row in dt.Rows)
+            //{
+            //    Console.WriteLine($"{row[0].ToString()}  {row[1].ToString()}");                
+            //}
+
+            Rootobject result = JsonConvert.DeserializeObject<Rootobject>(json);
+            //Rootobject result = JsonSerializer.Serialize<>.DeserializeObject<Rootobject>(json);
+            //foreach (DataArray item in result.Data)
+            //{
+            //    Console.WriteLine($"{item.name}  {item.language}");
+            //}
+
+            result.Name = result.Name + "___123";
+
+            string json2 = JsonConvert.SerializeObject(result, Formatting.Indented);
+            File.WriteAllText(@"C:\Users\байбатыровм\Desktop\json_sample_2_222.json", json2);
+        }
+    }
+}
