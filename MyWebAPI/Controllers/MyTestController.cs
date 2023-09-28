f4054dce18fc4cadf9c4ccd5a7ccff7a14481d95
mbaibatyr <murat_b@mail.ru>
2023-09-28 20:50:22 +0600

    -

---
+++ MyWebAPI/Controllers/MyTestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyTestController : ControllerBase
+    {
+        [HttpGet]
+        public string Hello(string name)
+        {
+            return "Hello " + name
+        }
+    }
+}
