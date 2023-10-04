b4035edc21b5188a3a0822db3139c6ef7c872e9c
mbaibatyr <murat_b@mail.ru>
2023-10-04 20:29:08 +0600

    -

--- AddConsole/Program.cs
+++ AddConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Data;
 
 namespace AddConsole
@@ -15,11 +16,50 @@ namespace AddConsole
             }
         }
 
+        static void JsonToClass()
+        {
+            string content = File.ReadAllText(@"C:\Users\байбатыровм\Desktop\test2.json");
+            var result = JsonConvert.DeserializeObject<RootCity>(content);
+            foreach (Data item in result.data)
+            {
+                Console.WriteLine($"{item.id} - {item.name} ");
+            }
+        }
+
+        static void JsonToArray()
+        {
+            string content = File.ReadAllText(@"C:\Users\байбатыровм\Desktop\test.json");
+            JArray jsonArray = JArray.Parse(content);
+            foreach (var item in jsonArray)
+            {
+                Console.WriteLine($"{item["id"].ToString()} - {item["name"].ToString()} ");
+            }
+        }
+
         public static void Main()
         {
-            JsonToDataTable();
-        }        
-    }    
+            //JsonToDataTable();
+            //JsonToClass();
+            JsonToArray();
+        }
+    }
+
+
+
+    public class RootCity
+    {
+        public Data[] data { get; set; }
+    }
+
+    public class Data
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+
+
+
 }
-    
+
 
