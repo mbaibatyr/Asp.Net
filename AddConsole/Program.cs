95671309cdeca0ac9be55fdb0f0ee12b11d1be92
mbaibatyr <murat_b@mail.ru>
2023-10-04 19:23:31 +0600

    --

---
+++ AddConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Data;
+
+namespace AddConsole
+{
+    public class Program
+    {
+        static void JsonToDataTable()
+        {
+            string content = File.ReadAllText(@"C:\Users\байбатыровм\Desktop\test.json");
+            DataTable dt = (DataTable)JsonConvert.DeserializeObject(content, (typeof(DataTable)));
+            foreach (DataRow item in dt.Rows)
+            {
+                Console.WriteLine($"{item[0].ToString()} - {item[1].ToString()} ");
+            }
+        }
+
+        public static void Main()
+        {
+            JsonToDataTable();
+        }        
+    }    
+}
+    
+
