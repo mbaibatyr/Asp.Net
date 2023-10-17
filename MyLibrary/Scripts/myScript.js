000d6632b0cb65850878e65131e9324e350970e1
mbaibatyr <murat_b@mail.ru>
2023-10-17 20:02:45 +0600

    ---

--- MyLibrary/Scripts/myScript.js
+++ MyLibrary/Scripts/myScript.js
@@ -1,5 +1,28 @@
+
+
 $(document).ready(function () {
-    $("#btTest").click(function () {
-        alert('hello step');
+    let array = [
+        {
+            "id": "1",
+            "name": "Almaty"
+        },
+        {
+            "id": "2",
+            "name": "Astana"
+        },
+    ]
+
+
+
+    //$("#Select1").change(function () {
+    //    var Select1 = $("#Select1").val();
+    //    alert(Select1);
+    //});
+
+    $("#btTest").click(function () {    
+        $("#Select1").empty();
+        for (let i = 0; i < array.length; i++) {
+            $("#Select1").append('<option value=' + array[i].id + '>' + array[i].name + '</option >');
+        };
     });
 });
