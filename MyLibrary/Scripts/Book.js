0f7b576382a38382b03ae7cd83c851c83d9ab315
mbaibatyr <murat_b@mail.ru>
2023-10-19 20:33:45 +0600

    --

--- MyLibrary/Scripts/Book.js
+++ MyLibrary/Scripts/Book.js
@@ -64,3 +64,67 @@ $(document).ready(function () {
         
     });
 });
+
+/*
+ 
+ create proc pBookGetAll
+@title nvarchar(100)
+as
+if @title = '' or @title is null or @title = 'all'
+	select	b.id,
+			b.title,
+			b.year,
+			ISNULL(a.first_name, ' ') + ISNULL(a.last_name, ' ') fio,
+			c.name category_name
+	from Book b join Author a on a.id = b.author_id
+	join Category c on c.id = b.category_id
+	order by b.title
+else
+	select	b.id,
+			b.title,
+			b.year,
+			ISNULL(a.first_name, ' ') + ISNULL(a.last_name, ' ') fio,
+			c.name category_name
+	from Book b join Author a on a.id = b.author_id
+	join Category c on c.id = b.category_id
+	where b.title like '%' + @title + '%'
+	order by b.title
+
+
+
+CREATE proc pBookAddOrEdit --4, N'test2', 1,  2000, 1
+@id          INT,
+@title       NVARCHAR (200),
+@author_id   INT,
+@year        INT,
+@category_id INT
+as
+	if @id = 0
+		begin
+			if exists(
+						select * from Book
+						where title = @title
+						and author_id = @author_id
+						)
+							begin 
+								select 'exists'
+								return
+							end 
+			insert into Book
+			(title, author_id, [year], category_id)
+			values
+			(@title, @author_id, @year, @category_id)
+			select 'ok'
+		end
+	else
+		begin
+			update Book
+				set title = @title, 
+				author_id = @author_id, 
+				[year] = @year, 
+				category_id = @category_id
+			where id = @id
+			--@@rowcount
+			select 'ok'
+		end
+ */
