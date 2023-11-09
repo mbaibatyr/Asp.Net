510c977930d3ea82b6ed8247bcbda3943a265e10
mbaibatyr <murat_b@mail.ru>
2023-11-09 20:04:00 +0600

    --

--- MyBasicAuth/Service/BasicAuth.cs
+++ MyBasicAuth/Service/BasicAuth.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Text.Encodings.Web;
 
 namespace MyBasicAuth.Service
 {
     public class BasicAuth : AuthenticationHandler<AuthenticationSchemeOptions>
     {
-      
+        public BasicAuth(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Missing Authorization Header");
+
+            var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+            var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+            var credentials = Encoding.UTF8.GetString(credentialBytes).Split(new[] { ':' }, 2);
+            var username = credentials[0];
+            var password = credentials[1];
+
+            throw new NotImplementedException();
+        }
     }
 }
