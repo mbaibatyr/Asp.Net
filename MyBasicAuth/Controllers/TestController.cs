58272130439b9fbaec37e7ffd875779d5162dfea
mbaibatyr <murat_b@mail.ru>
2023-11-09 21:19:54 +0600

    --

--- MyBasicAuth/Controllers/TestController.cs
+++ MyBasicAuth/Controllers/TestController.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyBasicAuth.Controllers
 {
     [Route("[controller]")]
     [ApiController]
+    [Authorize]
     public class TestController : ControllerBase
     {
-        [HttpGet, Route("getHello")]
+        [HttpGet, Route("getHello/{name}")]
         public ActionResult getHello(string name)
         {
             return Ok("Hello " + name);
