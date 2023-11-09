33240b9e122ef193a9f451de973f031d63356fa6
mbaibatyr <murat_b@mail.ru>
2023-11-09 21:04:08 +0600

    ---

---
+++ MyBasicAuth/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBasicAuth.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet, Route("getHello")]
+        public ActionResult getHello(string name)
+        {
+            return Ok("Hello " + name);
+        }
+    }
+}
