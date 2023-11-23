eaea2d0dc34f70bcaf2d7d127376f0f077e6f1b6
mbaibatyr <murat_b@mail.ru>
2023-11-23 19:45:45 +0600

    --

---
+++ MyEF/Model/City.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyEF.Model
+{
+    [Table("City")]
+    public class City
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MyEF/MyContext/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyEF.Model;
+
+namespace MyEF.MyContext
+{
+    public class Context : DbContext
+    {
+        public DbSet<City> City { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=223-P;Database=EF_CORE;Trusted_Connection=True;");
+        }
+    }
+}
