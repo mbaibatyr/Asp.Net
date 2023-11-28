da69249560e6f1758d15cd93278896872abedffc
mbaibatyr <murat_b@mail.ru>
2023-11-28 19:43:43 +0600

    ---

---
+++ MySOAP/SoapService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace MySOAP
+{
+    /// <summary>
+    /// Сводное описание для SoapService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Чтобы разрешить вызывать веб-службу из скрипта с помощью ASP.NET AJAX, раскомментируйте следующую строку. 
+    // [System.Web.Script.Services.ScriptService]
+    public class SoapService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld(string name)
+        {
+            return "Привет " + name;
+        }
+    }
+}
